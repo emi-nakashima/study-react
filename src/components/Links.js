7bcefd1e1a812f963f24d3956adf3296921cd262
emi nakashima <aoi1208rena@gmail.com>
2024-12-10 14:34:16 +0900

    ♻️ Linksのリファクタリング

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,32 +1,57 @@
 import Image from "next/image";
 import classes from "@/components/Links.module.css";
 
+const ITEMS = [
+  { 
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy now"
+  },
+  { 
+    href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Read our docs"
+  }
+];
+
 export function Links() {
   return (
+    <>
           <div className={classes.ctas}>
-            <a
-              className={classes.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className={classes.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={classes.secondary}
-            >
-              Read our docs
-            </a>
+            {ITEMS.map(item => {
+              return(
+                <a key={item.href}
+                  className={classes.primary}
+                  href={item.href}
+                  target="_blank" // リンクを新しいタブで開く設定
+                  rel="noopener noreferrer" // セキュリティ対策として、リンク先に現在のページ情報を渡さない
+                >
+                  <Image
+                    className={classes.logo}
+                    src="/vercel.svg" // 表示する画像ファイル（Vercelのロゴ）
+                    alt="Vercel logomark" // 画像が表示されない場合やスクリーンリーダー用の説明文
+                    width={20} // 画像の幅を20pxに設定
+                    height={20} // 画像の高さを20pxに設定
+                  />
+                  {item.title}
+                </a>
+              );
+            })}
           </div>
+
+          {/* <div className={classes.ctas}>
+            {ITEMS.map(item => {
+              return(
+                <a key={item.secondary}
+                  className={classes.secondary}
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.title}
+                </a>
+              );
+            })}
+          </div> */}
+          
+     </>
   );
 }
