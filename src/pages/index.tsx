23412823b3163a70230f453e824dd3a5642bfe27
emi nakashima <aoi1208rena@gmail.com>
2024-12-12 16:33:10 +0900

    ✨ useStateで配列を扱う

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,9 +18,10 @@ const geistMono = localFont({
 });
 
 export default function Home() {
-  const [foo, setFoo] = useState(1);
+  const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState<string[]>([]);
 
   // *** カウントアップの制御 *** //
   const handleClick = useCallback(() => {
@@ -42,6 +43,24 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
+  // *** インプットボックスに入力された文字をリストに追加 *** //
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します。");
+        inputRef.current?.select(); // 入力された文字を全選択
+        return prevArray;
+      } else {
+        setArray([...prevArray, text]);
+        setText(""); // 入力された文字をクリア
+        inputRef.current?.focus(); // インプットボックスにカーソルをフォーカス
+        return [...prevArray, text];
+      }
+    });
+  }, [text, setArray]);
+
   // *** バックカラーの変更 *** //
   useEffect(() => {
     // マウント時の処理
@@ -62,10 +81,16 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      {isShow ? <h1>{foo}</h1> : null}
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-      <input type="text" value={text} onChange={handleChange} />
+      <input ref={inputRef} type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
