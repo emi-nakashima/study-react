512739ae91afb9db2fd8b769a834d10de38d48f5
emi nakashima <aoi1208rena@gmail.com>
2024-12-12 11:57:17 +0900

    ✨ useStateで文字列と真偽値について解説

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,22 +19,40 @@ const geistMono = localFont({
 
 export default function Home() {
   const [foo, setFoo] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
+  // *** カウントアップの制御 *** //
   const handleClick = useCallback(() => {
     if (foo < 10) {
       setFoo((foo) => foo + 1);
     }
   }, [foo]);
 
-  useEffect(() => {
-    //マウント時の処理
-    document.body.style.backgroundColor = "lightblue";
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
+  // *** インプットボックスの制御 *** //
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内で入力してください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  // *** バックカラーの変更 *** //
+  useEffect(() => {
+    // マウント時の処理
+    document.body.style.backgroundColor = "lightblue";
     return () => {
-      //アンマウント時の処理
+      // アンマウント時の処理
       document.body.style.backgroundColor = "";
     };
   }, []);
+
+  // console.log(text);
 
   return (
     <div
@@ -44,9 +62,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{foo}</h1>
+      {isShow ? <h1>{foo}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
-
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
